40ca7bd2d333608a2f449df0a323c5dfbdd70fc9
manandakrishna <manandakrishna@gmail.com>
2025-02-22 22:39:49 +0800

    FirstVersion

---
+++ app/blogs/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+export default function BlogsPage() {
+  return (
+    <div>
+      <Typography variant="h4">Blogs Page</Typography>
+    </div>
+  );
+}
---
+++ app/keywords/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+export default function KeywoardsPage() {
+  return (
+    <div>
+      <Typography variant="h4">Keywords</Typography>
+    </div>
+  );
+}
